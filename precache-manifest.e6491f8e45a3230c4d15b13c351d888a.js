d5b5b12fd0d01ebfd6ffb5b5190772947e201a64
Untitled877 <1113425327@qq.com>
2021-09-02 11:58:44 +0800

    deploy

---
+++ precache-manifest.e6491f8e45a3230c4d15b13c351d888a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f16c7180318c815d5904",
+    "url": "/accountBook-vue-website/css/app.800df570.css"
+  },
+  {
+    "revision": "b28e91623db0eb910a31",
+    "url": "/accountBook-vue-website/css/chunk-vendors.38b0d5e0.css"
+  },
+  {
+    "revision": "37fb83a36178b32a54ac322c274ec3c1",
+    "url": "/accountBook-vue-website/fonts/haibao.37fb83a3.ttf"
+  },
+  {
+    "revision": "ff7e629702d1790226095430f58aa673",
+    "url": "/accountBook-vue-website/fonts/suxin.ff7e6297.ttf"
+  },
+  {
+    "revision": "e6d5ac154ae6f15300967fd966c7415f",
+    "url": "/accountBook-vue-website/fonts/zongyi.e6d5ac15.ttf"
+  },
+  {
+    "revision": "e0008d59ffad5ae239449558edfaad0c",
+    "url": "/accountBook-vue-website/index.html"
+  },
+  {
+    "revision": "f16c7180318c815d5904",
+    "url": "/accountBook-vue-website/js/app.8e62a214.js"
+  },
+  {
+    "revision": "b28e91623db0eb910a31",
+    "url": "/accountBook-vue-website/js/chunk-vendors.5ac1a5b3.js"
+  },
+  {
+    "revision": "3c2a68f5e35fe38c8ead66892ad53030",
+    "url": "/accountBook-vue-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/accountBook-vue-website/robots.txt"
+  }
+]);
